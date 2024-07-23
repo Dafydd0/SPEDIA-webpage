7da20a0437365ddc87fc8a12026138694a554286
Dafydd0 <davidalvarezbusiness@gmail.com>
2024-07-23 12:18:56 +0200

    contact info changed

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,122 +1,26 @@
-import { useState } from "react";
-import emailjs from "emailjs-com";
 import React from "react";
 
-const initialState = {
-  name: "",
-  email: "",
-  message: "",
-};
-
 export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState);
-  const [showThankYou, setShowThankYou] = useState(false);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setState((prevState) => ({ ...prevState, [name]: value }));
-  };
-
-  const clearState = () => setState({ ...initialState });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted:", name, email, message);
-
-    emailjs
-      .sendForm("service_0a3h8ma", "template_6fyu7cw", e.target, "t602HrzgJkXMmHvNN")
-      .then(
-        (result) => {
-          console.log("Email sent successfully:", result.text);
-          clearState();
-          setShowThankYou(true);
-          setTimeout(() => setShowThankYou(false), 10000); // Ocultar el mensaje después de 5 segundos
-        },
-        (error) => {
-          console.log("Error sending email:", error.text);
-        }
-      );
-  };
-
   return (
     <div>
       <div id="contact">
         <div className="container">
-          <div className="col-md-8">
+          <div className="col-md-12">
             <div className="row">
               <div className="section-title">
                 <h2>Contact us</h2>
-                <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
-                </p>
+                <p>You can reach out to us using the contact information below.</p>
               </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        placeholder="Name"
-                        required
-                        onChange={handleChange}
-                        value={name}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Email"
-                        required
-                        onChange={handleChange}
-                        value={email}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    rows="4"
-                    placeholder="Message"
-                    required
-                    onChange={handleChange}
-                    value={message}
-                  ></textarea>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div id="success"></div>
-                <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
-                </button>
-              </form>
-              {showThankYou && (
-                <div className="thank-you-message">
-                  <p>Thank you for contacting us. We will text you back as soon as possible.</p>
-                </div>
-              )}
             </div>
           </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
+          <div className="col-md-12 contact-info">
             <div className="contact-item">
               <h3>Contact Info</h3>
               <p>
                 <span>
                   <i className="fa fa-map-marker"></i> Address
                 </span>
-                {props.data ? props.data.address : "loading"}
+                {props.data ? props.data.address : " loading"}
               </p>
             </div>
             <div className="contact-item">
@@ -124,7 +28,7 @@ export const Contact = (props) => {
                 <span>
                   <i className="fa fa-phone"></i> Phone
                 </span>{" "}
-                {props.data ? props.data.phone : "loading"}
+                {props.data ? props.data.phone : " loading"}
               </p>
             </div>
             <div className="contact-item">
@@ -132,7 +36,7 @@ export const Contact = (props) => {
                 <span>
                   <i className="fa fa-envelope-o"></i> Email
                 </span>{" "}
-                {props.data ? props.data.email : "loading"}
+                {props.data ? props.data.email : " loading"}
               </p>
             </div>
           </div>
